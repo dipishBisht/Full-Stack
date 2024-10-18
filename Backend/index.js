169b99456fa9cfc2dffbf007257a1165b928ff4c
dipishBisht <dipish1718@gmail.com>
2024-10-18 16:39:44 +0530

    added cors

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require('express');
+const cors = require("cors")
 const connect = require("./connetion");
 const formRoute = require("./routes/form");
 const DB_URL = process.env.MONGO_URL;
@@ -7,9 +8,14 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-connect(DB_URL)
-
+app.use(cors({
+    origin: "https://mini-project-frontend-snowy.vercel.app/",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true
+}))
 app.use(express.json());
+
+connect(DB_URL)
 
 //# Form route middleware
 app.use("/form", formRoute)
